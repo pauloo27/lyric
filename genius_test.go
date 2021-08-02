6f59b59845b7996b51b95ab4cf684952fc084ba4
Paulo <pauloeduardo2709@gmail.com>
2021-08-02 15:01:18 -0300

    test: add DDG search test

--- genius_test.go
+++ genius_test.go
@@ -6,15 +6,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const freeSoftwareSong = "https://genius.com/Richard-stallman-free-software-song-lyrics"
+
 func TestGenius(t *testing.T) {
 	t.Run("Test fetch", func(t *testing.T) {
-		lyric, err := Fetch("https://genius.com/Richard-stallman-free-software-song-lyrics")
+		lyric, err := Fetch(freeSoftwareSong)
 		assert.Nil(t, err)
 		assert.NotEmpty(t, lyric)
 		assert.Contains(t, lyric, "You'll be free, hackers, you'll be free")
 		assert.Contains(t, lyric, "We'll kick out those dirty licenses")
 		assert.Contains(t, lyric, "[Chorus]")
 	})
-	// TODO: search duckduckgo
+	t.Run("Test duckduckgo search", func(t *testing.T) {
+		url, err := SearchDDG("free software song")
+		assert.Nil(t, err)
+		assert.NotNil(t, url)
+		assert.Equal(t, freeSoftwareSong, url)
+	})
 	// TODO: search genius
 }
