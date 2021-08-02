cc0dbc47457ea1ccc7d3eabea9b2e1095d2a36af
Paulo <pauloeduardo2709@gmail.com>
2021-08-02 14:37:32 -0300

    doc: add docs

--- genius.go
+++ genius.go
@@ -1,3 +1,4 @@
+// Package lyric can be used to search and fetch song lyrics
 package lyric
 
 import (
@@ -11,8 +12,8 @@ import (
 	"github.com/anaskhan96/soup"
 )
 
-var lyricURLRe = regexp.MustCompile(`https:\/\/genius.com/[^/]+-lyrics`)
-
+// Fetch fetchs a lyrics by it's url (the parameter path (not called url to
+// avoid problems with the url package)). It returns the lyric and an error.
 func Fetch(path string) (lyric string, err error) {
 	res, err := http.Get(path)
 	if err != nil {
@@ -56,6 +57,13 @@ func search(query string) (string, error) {
 	return "", nil
 }
 
+// used in the DuckDuckGO search
+var lyricURLRe = regexp.MustCompile(`https:\/\/genius.com/[^/]+-lyrics`)
+
+// SearchDDG searchs for a query using DuckDuckGO. Search engines can deal with
+// typos and not exact searchs. DuckDuckGO have a rate limit, so don't call the
+// same search too many times. It returns the URL of the lyrics and an error,
+// you can fetch the actual lyrics using Fetch(path).
 func SearchDDG(query string) (string, error) {
 	path := fmt.Sprintf("https://html.duckduckgo.com/html/?q=site:genius.com+%s", url.QueryEscape(query))
 
