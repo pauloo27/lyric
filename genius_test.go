984bfe7441faf50b413025ea649a9eebc9119511
Paulo <git@pauloo27.dev>
2023-05-14 23:01:21 -0300

    test: use lyric_test packages for tests

--- genius_test.go
+++ genius_test.go
@@ -1,8 +1,9 @@
-package lyric
+package lyric_test
 
 import (
 	"testing"
 
+	"github.com/pauloo27/lyric"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,7 +11,7 @@ const freeSoftwareSong = "https://genius.com/Richard-stallman-free-software-song
 
 func TestGenius(t *testing.T) {
 	t.Run("Test fetch", func(t *testing.T) {
-		lyric, err := Fetch(freeSoftwareSong)
+		lyric, err := lyric.Fetch(freeSoftwareSong)
 		assert.Nil(t, err)
 		assert.NotEmpty(t, lyric)
 		assert.Contains(t, lyric, "You'll be free, hackers, you'll be free")
@@ -18,13 +19,13 @@ func TestGenius(t *testing.T) {
 		assert.Contains(t, lyric, "[Chorus]")
 	})
 	t.Run("Test duckduckgo search", func(t *testing.T) {
-		url, err := SearchDDG("free software song")
+		url, err := lyric.SearchDDG("free software song")
 		assert.Nil(t, err)
 		assert.NotNil(t, url)
 		assert.Equal(t, freeSoftwareSong, url)
 	})
 	t.Run("Test genius search", func(t *testing.T) {
-		url, err := Search("free software song")
+		url, err := lyric.Search("free software song")
 		assert.Nil(t, err)
 		assert.NotNil(t, url)
 		assert.Equal(t, freeSoftwareSong, url)
